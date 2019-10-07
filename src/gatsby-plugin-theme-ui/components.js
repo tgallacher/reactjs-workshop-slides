944e5225de67f3e5dd763020d944c93612839716
Tom Gallacher <tgallacher@users.noreply.github.com>
2019-10-07 20:54:06 +0100

    minor style tweaks

--- src/gatsby-plugin-theme-ui/components.js
+++ src/gatsby-plugin-theme-ui/components.js
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
-import React from 'react';
+import React from 'react'; // eslint-disable-line
 import Prism from '@theme-ui/prism';
 import {
   Appear,
@@ -10,12 +10,29 @@ import {
 } from 'gatsby-theme-mdx-deck';
 
 import ReactSpinner from '../components/SpinReactLogo.jsx';
-import Callout from '../components/Callout.jsx';
+// import Callout from '../components/Callout.jsx';
 
 const Emoji = ({ className, sx, ...props }) => (
   <div
     sx={{ mt: 4, fontSize: 6, ...sx }}
     className={'em '.concat(className)}
+    {...props}
+  />
+);
+
+const Callout = ({ sx, ...props }) => (
+  <div
+    sx={{
+      borderWidth: 0,
+      borderLeftColor: 'secondary',
+      borderLeftWidth: 6,
+      borderStyle: 'solid',
+      backgroundColor: 'muted',
+      p: 4,
+      mt: 4,
+      fontWeight: 'bold',
+      ...sx,
+    }}
     {...props}
   />
 );
